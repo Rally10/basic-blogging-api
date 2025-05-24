b2bc83e9e8be8b82df6e87c12c4734c63a411db5
Rally10 <bravo1017@proton.me>
2025-05-24 01:02:54 -0500

    server definition

---
+++ index.js
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import firstRoute from './routes/first-route.js'
+
+const fastify = new Fastify({
+  logger: true
+})
+
+fastify.register(firstRoute)
+
+fastify.listen({ port: 3000, host: '192.168.100.2' }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
